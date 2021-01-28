df195dcccfbd3307a948a66cfaf564f16c7acc5e
Aftertaste26 <20hardrock12@gmail.com>
2021-01-28 10:17:31 +0800

    feat: missing two pair checker added

--- poke.js
+++ poke.js
@@ -2,22 +2,18 @@ const isConsecutive = (cards) => {
   const letterCard = { A: "1", J: "11", Q: "12", K: "13" };
 
   return cards
-    .map((card) =>
-      Object.keys(letterCard).includes(card) ? letterCard[card] : card
-    )
+    .map((card) => letterCard[card] || card)
     .sort((a, b) => a - b)
     .every((card, index, array) => {
       return Math.abs(card - (array[index + 1] || array[index - 1])) === 1;
     });
 };
 
-const tally = (cards) => {
-  const output = {};
-  cards.forEach((card) => {
-    output[card] = (output[card] || 0) + 1;
-  });
-  return output;
-};
+const tally = (cards) =>
+  cards.reduce((list, card) => {
+    list[card] = (list[card] || 0) + 1;
+    return list;
+  }, {});
 
 const hasMatch = (count, cards) => Object.values(tally(cards)).includes(count);
 
@@ -45,6 +41,8 @@ const poker = (cards) => {
   if (isConsecutive(cards)) return "Straight";
 
   if (hasMatch(3, cards)) return "Three of a Kind";
+
+  if (tally(Object.values(tally(cards)))[2] == 2) return "Two Pair";
 
   if (hasMatch(2, cards)) return "One Pair";
 
